d6e48b35a2f967f7e28fde48db7f6b2834c41de5
agungrhiz <39258850+agungrhiz@users.noreply.github.com>
2023-07-22 16:28:35 +0700

    add authentication apollo

--- client/src/lib/apollo-wrapper.tsx
+++ client/src/lib/apollo-wrapper.tsx
@@ -1,41 +1,45 @@
 "use client";
 
 import { ApolloLink, HttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import {
   ApolloNextAppProvider,
   NextSSRInMemoryCache,
   NextSSRApolloClient,
   SSRMultipartLink,
 } from "@apollo/experimental-nextjs-app-support/ssr";
+import { getCookie } from "cookies-next";
 
 function makeClient() {
   const httpLink = new HttpLink({
     uri: process.env.NEXT_PUBLIC_GRAPHQL_URL,
-
-    // you can disable result caching here if you want to
-    // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
     fetchOptions: { cache: "no-store" },
   });
 
+  const authLink = setContext((_, { headers }) => {
+    const token = getCookie("access_token");
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    };
+  });
+
   return new NextSSRApolloClient({
-    // use the `NextSSRInMemoryCache`, not the normal `InMemoryCache`
     cache: new NextSSRInMemoryCache(),
     link:
       typeof window === "undefined"
         ? ApolloLink.from([
-            // in a SSR environment, if you use multipart features like
-            // @defer, you need to decide how to handle these.
-            // This strips all interfaces with a `@defer` directive from your queries.
             new SSRMultipartLink({
               stripDefer: true,
             }),
-            httpLink,
+            authLink.concat(httpLink),
           ])
-        : httpLink,
+        : authLink.concat(httpLink),
   });
 }
 
-// you need to create a component to wrap your app in
 export function ApolloWrapper({ children }: React.PropsWithChildren) {
   return (
     <ApolloNextAppProvider makeClient={makeClient}>
--- client/src/lib/client.ts
+++ client/src/lib/client.ts
@@ -1,15 +1,25 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
+import { getCookie } from "cookies-next";
 
 export const { getClient } = registerApolloClient(() => {
+  const httpLink = new HttpLink({
+    uri: process.env.NEXT_PUBLIC_GRAPHQL_URL,
+  });
+
+  const authLink = setContext((_, { headers }) => {
+    const token = getCookie("access_token");
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    };
+  });
+
   return new ApolloClient({
     cache: new InMemoryCache(),
-    link: new HttpLink({
-      // this needs to be an absolute url, as relative urls cannot be used in SSR
-      uri: process.env.NEXT_PUBLIC_GRAPHQL_URL,
-      // you can disable result caching here if you want to
-      // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
-      // fetchOptions: { cache: "no-store" },
-    }),
+    link: authLink.concat(httpLink),
   });
 });
